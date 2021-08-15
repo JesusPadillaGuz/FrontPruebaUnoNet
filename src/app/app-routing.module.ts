8ce7db78e3bc31ecb5e719320f60e48613a86653
Jose de Jesus Padilla Guzman <chuy_student23@outlook.com>
2021-08-15 16:15:15 -0500

    front PruebaUnoNet terminado

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PersonasControlComponent } from './authentication-pages/personas/personas-control/personas-control.component';
+import { PersonasEditarComponent } from './authentication-pages/personas/personas-editar/personas-editar.component';
+import { PersonasNuevoComponent } from './authentication-pages/personas/personas-nuevo/personas-nuevo.component';
+import { PrincipalComponent } from './authentication-pages/principal/principal.component';
+import { LoginComponent } from './login/login.component';
+const routes: Routes = [
+  { path: 'login' , component:  LoginComponent},
+  { path: 'principal' , component:  PrincipalComponent},
+  { path: 'personasFisicas' , component:  PersonasControlComponent},
+  { path: 'personasFisicas/Nuevo' , component:  PersonasNuevoComponent},
+  { path: 'personasFisicas/Editar/:id' , component:  PersonasEditarComponent},
+  { path: '**', redirectTo: 'login' },
+  { path: ' ', pathMatch: 'full', redirectTo: 'login' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PrincipalComponent } from './authentication-pages/principal/principal.component';
+import { PersonasControlComponent } from './authentication-pages/personas/personas-control/personas-control.component';
+import { PersonasNuevoComponent } from './authentication-pages/personas/personas-nuevo/personas-nuevo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PersonasEditarComponent } from './authentication-pages/personas/personas-editar/personas-editar.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    PrincipalComponent,
+    PersonasControlComponent,
+    PersonasNuevoComponent,
+    PersonasEditarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    BrowserAnimationsModule, 
+    ToastrModule.forRoot({
+      timeOut: 3500,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
